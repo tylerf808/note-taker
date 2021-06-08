c4e86727d9b66f73fd9b104bc80290d9884496c0
Tyler Fabian <tylerf808@gmail.com>
2021-06-08 15:04:08 -0400

    Fixed server and added html paths

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const PORT = 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const server = http.createServer(handleRequest);
-
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
