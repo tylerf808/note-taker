beebe21f86de5e171d7684700bd2fa0966ce7cb1
Tyler Fabian <tylerf808@gmail.com>
2021-06-10 16:26:54 -0400

    Finished get notes api method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,13 @@
-module.exports = (app) => {
+const fs = require('fs');
 
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) => {
+        const data = fs.readFileSync('../../develop/note-taker/db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+        })
+        res.json(data);
+    })
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = (app) => {
     app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
+        res.sendFile(path.join(__dirname), '../public/notes.html')
     })
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
