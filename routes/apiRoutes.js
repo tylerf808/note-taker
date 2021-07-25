8f4d97737baf4b68e7bf0570ad467c560de64ddb
Tyler Fabian <tylerf808@gmail.com>
2021-07-25 18:31:48 -0400

    Fixed routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,15 @@
 const db = require('../db/db.json')
 const fs = require('fs');
+const { json } = require('express');
 
 module.exports = (app) => {
+
     app.get('/api/notes', (req, res) => {
         res.json(db);
+    })
+
+    app.post('/api/notes', (req, res) => {
+        
+        console.log(notes)
     })
 }
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
 const PORT = 3001;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(express.json());
 
 require('./routes/apiRoutes')(app);
