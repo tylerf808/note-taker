900f3ecc55f96c86f30bda4851266b215d57c882
Tyler Fabian <tylerf808@gmail.com>
2021-07-25 17:21:37 -0400

    Reorganized files and format

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,8 @@
+const db = require('../db/db.json')
 const fs = require('fs');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
-        const data = fs.readFileSync('../../develop/note-taker/db/db.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error(err)
-                return
-            }
-        })
-        const notes = JSON.parse(data);
-        res.json(notes);
+        res.json(db);
     })
 }
